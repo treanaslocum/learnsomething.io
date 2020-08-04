48682eaca433e7ef5dd05fa695f320f9858ba1de
Treana Slocum <almightyolmec@gmail.com>
2020-08-04 12:48:54 -0700

    test statcounter addition

--- pages/index.vue
+++ pages/index.vue
@@ -55,6 +55,23 @@
     </section>
 
     <Footer/>
+    <!-- Default Statcounter code for Learnsomething.io
+    http://www.learnsomething.io -->
+    <script type="text/javascript">
+    var sc_project=12370753; 
+    var sc_invisible=1; 
+    var sc_security="429f25a4"; 
+    var sc_https=1; 
+    </script>
+    <script type="text/javascript"
+    src="https://www.statcounter.com/counter/counter.js"
+    async></script>
+    <noscript><div class="statcounter"><a title="Web Analytics"
+    href="https://statcounter.com/" target="_blank"><img
+    class="statcounter"
+    src="https://c.statcounter.com/12370753/0/429f25a4/1/"
+    alt="Web Analytics"></a></div></noscript>
+    <!-- End of Statcounter Code -->
   </div>
 </template>
 
