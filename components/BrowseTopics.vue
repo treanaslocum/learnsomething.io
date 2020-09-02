63f53c8952b3c703539ab8e51ad4348f148691dc
Treana Slocum <almightyolmec@gmail.com>
2020-09-02 11:02:32 -0700

    Added teachers and their pages

--- components/BrowseTopics.vue
+++ components/BrowseTopics.vue
@@ -350,8 +350,9 @@
                Photography
                 <!-- <svg class="ml-1 fill-current text-red-500" xmlns="http://www.w3.org/2000/svg" width="15" height="24" viewBox="0 0 24 24"><path d="M12.579,2.393c-0.349-0.248-0.808-0.245-1.153,0.006C9.592,3.728,4,8.252,4,14c0,3.247,1.948,6.043,4.734,7.296 C8.275,20.645,8,19.855,8,19c-0.017-3.221,3.558-6.893,3.71-7c0.173-0.124,0.407-0.124,0.579,0C12.441,12.107,16,14.974,16,19.002 c0,0.854-0.275,1.643-0.733,2.294C18.052,20.043,20,17.248,20,14.005C20,8.144,14.418,3.698,12.579,2.393z"/></svg> -->
               </nuxt-link>
-              <nuxt-link to="/topic/illustration" class="flex pointer-events-none text-grey-100 mb-2">
+              <nuxt-link to="/topic/illustration" class="flex items-center text-black-900 hover:text-black-800 mb-2">
               Illustration
+              <div class="ml-3 bg-yellow-500 text-white text-xs font-semibold rounded uppercase px-2">in progress</div>
              </nuxt-link>
              <nuxt-link to="/topic/digital-illustration" class="flex pointer-events-none text-grey-100 mb-2">
               Digital Illustration
