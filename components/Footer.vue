eab419b1a9310bd19e7eaa2b6e638d7638324215
Treana Slocum <treanaslocum.com>
2023-08-25 20:37:04 -0700

    Create new footer design

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,46 @@
 <template>
   <div>
-    <footer class="mt-40 mb-8 px-20">
+    <footer class="mb-16 mt-40">
+      <div class="flex mx-32 -mb-20">
+        <div class="mr-40">
+          <svg class="mb-4" xmlns="http://www.w3.org/2000/svg" width="130" height="50" viewBox="0 0 205.581 53">
+              <g id="Group_22" data-name="Group 22" transform="translate(-856.998 -47)">
+                <g id="Group_10" data-name="Group 10" transform="translate(768.022 -89)">
+                <g id="Group_9" data-name="Group 9">
+                  <path id="Rectangle_67" data-name="Rectangle 67" d="M3.15,0h12.6A15.748,15.748,0,0,1,31.5,15.748V21A15.748,15.748,0,0,1,15.748,36.745h0A15.748,15.748,0,0,1,0,21V3.15A3.15,3.15,0,0,1,3.15,0Z" transform="translate(133.512 144.262) rotate(68)"/>
+                  <text id="s" transform="matrix(0.174, 0.985, -0.985, 0.174, 108.672, 155.226)" fill="#fff" font-size="48" font-family="Heebo-Bold, Heebo" font-weight="700"><tspan x="0" y="0">s</tspan></text>
+                </g>
+                <text id="skillseed" transform="translate(149.557 174)" font-size="36" font-family="Heebo-Bold, Heebo" font-weight="700"><tspan x="0" y="0">skillseed</tspan></text>
+              </g>
+            </g>
+          </svg>
+          <p>Skillseed is the best place<br> to learn anything.</p>
+        </div>
+        <div class="flex mb-40">
+          <div class="mr-40">
+            <h4 class="font-semibold mb-4">For Learners</h4>
+            <nuxt-link to="/">Find a playlist</nuxt-link>
+          </div>
+          <div>
+            <h4 class="font-semibold mb-4">Company</h4>
+            <nuxt-link to="/">Mission</nuxt-link>
+          </div>
+        </div>
+      </div>
+      
+      <div class="flex mt-20 mx-32 justify-between">
+        <div>
+          <p>© Skillseed, LLC 2023</p>
+        </div>
+        <div>
+          <p class="text-blue-900">Made with <span class="text-black">❤</span> in Las Vegas. NV</p>
+        </div>
+        <div>
+          <p><span class="font-semibold">48,000</span> playlists watched</p>
+        </div>
+      </div>
+    </footer>
+    <!-- <footer class="mt-40 mb-8 px-20">
       <div class="flex items-center justify-between">
         <nuxt-link to="/">
           <svg class="w-40" xmlns="http://www.w3.org/2000/svg" width="209.5" height="47.5" viewBox="0 0 209.5 47.5"><defs><style>.a,.c{fill:#2f2e41;}.b{fill:#fff;font-size:40px;font-family:WorkSans-Medium, Work Sans;font-weight:500;}.c{font-size:20px;font-family:WorkSans-Regular, Work Sans;}</style></defs><g transform="translate(-107.5 -53.5)"><path class="a" d="M19.162,7.568a5,5,0,0,1,8.676,0L42.709,33.514A5,5,0,0,1,38.371,41H8.629a5,5,0,0,1-4.338-7.486Z" transform="translate(149.5 54) rotate(90)"/><text class="b" transform="translate(107.5 90.5)"><tspan x="0" y="0">ls</tspan></text><text class="c" transform="translate(157 84)"><tspan x="0" y="0">LearnSomething</tspan></text></g></svg>
@@ -12,6 +52,6 @@
         <p class="text-blue-900">Made with <span class="text-red-500">❤</span> in Las Vegas</p>
         <p class="text-blue-900">©2020. All Rights Reserved.</p>
       </div>
-    </footer>
+    </footer> -->
   </div>
 </template>
