717e16206a212cda4700fb1fba0aa2807747000f
Treana Slocum <treanaslocum.com>
2023-08-26 16:38:19 -0700

    change topics page

--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -3,7 +3,398 @@
     <!-- multi-language banner -->
     <!-- <MultiLanguageSupport/> -->
     <Header/>
-    <BrowseTopics/>
+    
+    <div class="flex m-20">
+
+      <!-- stem section -->
+      <div>
+        <div class="w-3/4 mb-8">
+          <h3 class="text-lg font-semibold">S.T.E.M</h3>
+          <p>Learn Science, Technology, Engineering and Mathematics</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Cybersecurity</nuxt-link>
+          </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Cryptography</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Computer Networking</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >HTML & CSS</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Java</nuxt-link>
+         </div>
+         <div class="flex">
+           <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >JavaScript</nuxt-link>
+          </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Ruby</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Ruby on Rails</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >PHP</nuxt-link>
+         </div>
+         <div class="fle">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Python</nuxt-link>
+         </div>
+         <div class="fle">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Laravel</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >C++</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >SQL</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Command Line</nuxt-link>
+         </div>
+         <div class="fle">
+          <span class="font-bold mr-2"><nuxt-link to="/cs">CS</nuxt-link></span> <nuxt-link to="/" >Git</nuxt-link>
+         </div>
+          
+        </div>
+      </div>
+      
+      <!-- art and design section -->
+      <div>
+        <div class="mb-8">
+          <h3 class="text-lg font-semibold">Art & Design</h3>
+          <p class="w-3/4">Create new and wonderful things by learning art and design.</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/art">ART</nuxt-link></span> <nuxt-link to="/" >Painting</nuxt-link>
+          </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/art">ART</nuxt-link></span> <nuxt-link to="/" >Photography</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/art">ART</nuxt-link></span> <nuxt-link to="/" >Drawing</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/art">ART</nuxt-link></span> <nuxt-link to="/" >Illustration</nuxt-link>
+         </div>
+         <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/design">DESG</nuxt-link></span> <nuxt-link to="/" >Adobe Photoshop</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/design">DESG</nuxt-link></span> <nuxt-link to="/" >Concept Art</nuxt-link>
+         </div>
+
+          
+        </div>
+      </div>
+      
+      
+      <!-- Languages section -->
+      <div>
+        <div class="mb-8">
+          <h3 class="text-lg font-semibold">Languages</h3>
+          <p>Become a polyglot and learn multiple languages.</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <h3 class="font-semibold mb-4">Latin America</h3>
+          <div class="flex mb-6">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Spanish</nuxt-link>
+          </div>
+
+          <h3 class="font-semibold mt-4 mb-4">Asia</h3>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Korean</nuxt-link>
+          </div>
+          <div class="flex mb-6">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Japanese</nuxt-link>
+          </div>
+
+          <h3 class="font-semibold mt-4 mb-4">Africa</h3>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Yoruba</nuxt-link>
+          </div>
+          <div class="flex mb-6">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Swahili</nuxt-link>
+          </div>
+      
+
+          <h3 class="font-semibold mt-4 mb-4">Europe</h3>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >French</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Italian</nuxt-link>
+          </div>
+          <div class="flex mb-6">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >German</nuxt-link>
+          </div>
+
+          <h3 class="font-semibold mt-4 mb-4">Middle East</h3>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Arabic</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Pashto</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Hebrew</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Farsi</nuxt-link>
+          </div>
+          <div class="flex mb-6">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Turkish</nuxt-link>
+          </div>
+
+          <h3 class="font-semibold mt-4 mb-4">Other</h3>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/languages">LANG</nuxt-link></span> <nuxt-link to="/" >Sign Language</nuxt-link>
+          </div>
+          
+        </div>
+      </div>
+      
+      <!-- Essential Life Skill section -->
+      <div>
+        <div class="mb-8">
+          <h3 class="text-lg font-semibold">Essential Life Skills</h3>
+          <p>Learn the skills everyone should know.</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Crafts</nuxt-link>
+          </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Clothes</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Building</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Organizing</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Renovations</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Engineering</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/diy">DIY</nuxt-link></span> <nuxt-link to="/" >Decorating</nuxt-link>
+         </div>
+
+         <!-- category divider -->
+         <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/survival">SURV</nuxt-link></span> <nuxt-link to="/" >Hunting</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/survival">SURV</nuxt-link></span> <nuxt-link to="/" >Fishing</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/survival">SURV</nuxt-link></span> <nuxt-link to="/" >Canning</nuxt-link>
+         </div>
+
+         <!-- category divider -->
+         <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/combat">COMB</nuxt-link></span> <nuxt-link to="/" >Krav Maga</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/combat">COMB</nuxt-link></span> <nuxt-link to="/" >Boxing</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/combat">COMB</nuxt-link></span> <nuxt-link to="/" >Brazilian Jiu Jitsu</nuxt-link>
+         </div>
+
+         <!-- category divider -->
+         <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/relationship">RELA</nuxt-link></span> <nuxt-link to="/" >Dating</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/relationship">RELA</nuxt-link></span> <nuxt-link to="/" >Public Speaking</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/relationship">RELA</nuxt-link></span> <nuxt-link to="/" >Family</nuxt-link>
+         </div>
+
+         <!-- category divider -->
+         <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >Korean</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >Italian</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >American</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >Indian</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >Mexican</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cooking">COOK</nuxt-link></span> <nuxt-link to="/" >Japanese</nuxt-link>
+         </div>
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+          <span class="font-bold mr-2"><nuxt-link to="/cleaning">CLEA</nuxt-link></span> <nuxt-link to="/" >Bathroom</nuxt-link>
+         </div>
+         <div class="flex">
+          <span class="font-bold mr-2"><nuxt-link to="/cleaning">CLEA</nuxt-link></span> <nuxt-link to="/" >Kitchen</nuxt-link>
+         </div>
+       
+          
+        </div>
+      </div>
+
+
+      <!-- self care section -->
+      <div>
+        <div class="mb-8">
+          <h3 class="text-lg font-semibold">Self Care</h3>
+          <p>Treat yourself how you should be treated</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/skin">SKIN</nuxt-link></span> <nuxt-link to="/" >Facial</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/skin">SKIN</nuxt-link></span> <nuxt-link to="/" >Body</nuxt-link>
+          </div>
+          
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/mental-health">MENT</nuxt-link></span> <nuxt-link to="/" >Meditation</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/mental-health">MENT</nuxt-link></span> <nuxt-link to="/" >Therapy</nuxt-link>
+          </div>
+
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/fitness">FIT</nuxt-link></span> <nuxt-link to="/" >Calisthenics & Bodyweight</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/fitness">FIT</nuxt-link></span> <nuxt-link to="/" >Weight Lifting</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/fitness">FIT</nuxt-link></span> <nuxt-link to="/" >Running</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/fitness">FIT</nuxt-link></span> <nuxt-link to="/" >Walking</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/fitness">FIT</nuxt-link></span> <nuxt-link to="/" >Jump Roping</nuxt-link>
+          </div>
+
+           <!-- category divider -->
+           <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/hair">HAIR</nuxt-link></span> <nuxt-link to="/" >Curly Hair</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/hair">HAIR</nuxt-link></span> <nuxt-link to="/" >Coily Hair</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/hair">HAIR</nuxt-link></span> <nuxt-link to="/" >Straight Hair</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/hair">HAIR</nuxt-link></span> <nuxt-link to="/" >Wavy Hair</nuxt-link>
+          </div>
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/nutrition">NUTR</nuxt-link></span> <nuxt-link to="/" >Keto</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/nutrition">NUTR</nuxt-link></span> <nuxt-link to="/" >Vegan</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/nutrition">NUTR</nuxt-link></span> <nuxt-link to="/" >Vegetarian</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/nutrition">NUTR</nuxt-link></span> <nuxt-link to="/" >Paleo</nuxt-link>
+          </div>
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/hygiene">HYGN</nuxt-link></span> <nuxt-link to="/" >Feminine Care</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/hygiene">HYGN</nuxt-link></span> <nuxt-link to="/" >Grooming</nuxt-link>
+          </div>
+          
+        </div>
+      </div>
+
+      <!-- business and finance section -->
+      <div>
+        <div class="mb-8">
+          <h3 class="text-lg font-semibold">Business & Finance</h3>
+          <p>Learn business and finance</p>
+        </div>
+        <div class="mb-8 border-b-2 border-black w-12"></div>
+
+        <div class="text-sm flex flex-col">
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/business">BUS</nuxt-link></span> <nuxt-link to="/" >Entrepreneurship</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/business">BUS</nuxt-link></span> <nuxt-link to="/" >Funding</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/business">BUS</nuxt-link></span> <nuxt-link to="/" >Startup</nuxt-link>
+          </div>
+          
+          
+
+          <!-- category divider -->
+          <div class="flex mt-4">
+            <span class="font-bold mr-2"><nuxt-link to="/finances">FIN</nuxt-link></span> <nuxt-link to="/" >Futures</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/finances">FIN</nuxt-link></span> <nuxt-link to="/" >Stocks</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/finances">FIN</nuxt-link></span> <nuxt-link to="/" >Forex</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/finances">FIN</nuxt-link></span> <nuxt-link to="/" >Options</nuxt-link>
+          </div>
+          <div class="flex">
+            <span class="font-bold mr-2"><nuxt-link to="/finances">FIN</nuxt-link></span> <nuxt-link to="/" >Crypto</nuxt-link>
+          </div>
+
+
+
+         
+
+          
+        </div>
+      </div>
+      
+    </div>
+
+
+
+
+
     <Footer/>
     <!-- Default Statcounter code for Learnsomething.io
     http://www.learnsomething.io -->
